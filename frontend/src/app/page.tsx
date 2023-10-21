d09d625de76fe00654aeb135d9834df8a5b373d1
Alvaro <65318965+alvarovalverde03@users.noreply.github.com>
2023-10-21 15:43:08 +0200

    Add redirection from / to /chats

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,8 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
+  redirect('/chats')
+
   return (
     <main className="flex h-full flex-row items-center p-3">
       <div className="h-full max-w-screen-md mx-auto flex w-full">
